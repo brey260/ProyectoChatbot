779cf264884151582332887ae380bf15d334d3fc
bgarciac516 <bgarciac516@gmail.com>
2025-11-20 18:28:04 -0500

    correcion final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Chat from './components/Chat';
-
+// app
 export default function App() {
     return (
         <div className="w-full h-screen bg-linear-to-br from-slate-950 via-slate-950 to-slate-900">
